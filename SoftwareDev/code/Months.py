5afa7b4804d84e891b3788a09a11d0058a638f78
20220819 <110364569+20220819@users.noreply.github.com>
2022-08-11 14:25:54 +1200

    Update Months.py

--- SoftwareDev/code/Months.py
+++ SoftwareDev/code/Months.py
@@ -1,11 +1,29 @@
-month = input("Input the name of the month:")
-days = 0 #days set to 0 by default
+# month = input("Input the name of the month:")
+# days = 0 #days set to 0 by default
 
-if(month == "February"):
-    days = "28/29"
-elif(month == "April" and "June" and "September" and "November"):
-    days = 30
-elif(month == "January" and "March" and "May" and "July" and "August" and "October" and "December"):
-    days = 31
+# if(month == "February"):
+#     days = "28/29"
+# elif(month == "April" "June" and/or "September" and/or "November"):
+#     days = 30
+# elif(month == "January" and "March" and "May" and "July" and "August" and "October" and "December"):
+#     days = 31
     
-print("No. of days:", days)
+# print("No. of days:", days)
+
+a = ["April", "June", "September", "November"] #30 days
+b = ["January", "March", "May", "July", "August", "October", "December"] #31 days
+c = ["February"] #28 or 29 days
+
+month = input("Input name of the Month:")
+
+for i in a:
+    if i == month :
+        print("No. Of Days: 30")
+
+for i in b:
+    if i == month :
+        print("No. Of Days: 31")
+        
+for i in c:
+    if i == month :
+        print("No. Of Days: 28/29")      

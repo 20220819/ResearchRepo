1c6ba2de5ea65b737b383997e7e6ffa9ff9b8f72
20220819 <110364569+20220819@users.noreply.github.com>
2022-08-11 13:30:33 +1200

    Update Months.py

--- SoftwareDev/code/Months.py
+++ SoftwareDev/code/Months.py
@@ -2,7 +2,7 @@ month = input("Input the name of the month:")
 days = 0 #days set to 0 by default
 
 if(month == "February"):
-    days = "28 or 29"
+    days = "28/29"
 elif(month == "April" and "June" and "September" and "November"):
     days = 30
 elif(month == "January" and "March" and "May" and "July" and "August" and "October" and "December"):
